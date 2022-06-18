5cb1ba691bf3a0cbad0366cbe673aca3446d658b
Singh36 <singh.dhaliwal36@gmail.com>
2022-06-18 17:28:43 +1000

    updated the code

--- src/main/java/com/learning/demo/HelloWorld.java
+++ src/main/java/com/learning/demo/HelloWorld.java
@@ -2,7 +2,10 @@ package com.learning.demo;
 
 public class HelloWorld {
 
+    int x = 5;
+
     public static void main(String[] args) {
-        System.out.println("Salute To Ramana Maharshi");
+        HelloWorld myobj = new HelloWorld();
+        System.out.println(myobj.x);
     }
 }
