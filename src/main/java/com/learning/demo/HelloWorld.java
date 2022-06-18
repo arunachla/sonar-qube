10fb45f214eccf2ada86566c94f6a293a709904c
Singh36 <singh.dhaliwal36@gmail.com>
2022-06-18 18:58:27 +1000

    Updated code in feature branch

--- src/main/java/com/learning/demo/HelloWorld.java
+++ src/main/java/com/learning/demo/HelloWorld.java
@@ -3,9 +3,10 @@ package com.learning.demo;
 public class HelloWorld {
 
     int x = 5;
+    int y = 10;
 
     public static void main(String[] args) {
         HelloWorld myobj = new HelloWorld();
-        System.out.println(myobj.x);
+        System.out.println(myobj.x + myobj.y);
     }
 }
