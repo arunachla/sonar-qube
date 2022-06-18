9e7d191f79db91ef84027f4e05db3b0eab056cd3
Singh36 <singh.dhaliwal36@gmail.com>
2022-06-18 19:52:56 +1000

    Updated code again

--- src/main/java/com/learning/demo/HelloWorld.java
+++ src/main/java/com/learning/demo/HelloWorld.java
@@ -4,9 +4,10 @@ public class HelloWorld {
 
     int x = 5;
     int y = 10;
+    int z = 20;
 
     public static void main(String[] args) {
         HelloWorld myobj = new HelloWorld();
-        System.out.println(myobj.x + myobj.y);
+        System.out.println(myobj.x + myobj.y + myobj.z);
     }
 }
